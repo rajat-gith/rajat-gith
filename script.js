b840620b827c449155c819b909718f1c29823b30
Rajat Kumar Nayak <rajatnayak1582002@gmail.com>
2022-06-22 20:14:08 +0530

    commit

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-const brand_tittle=document.getElementsByClassName('brand-title')[0]
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
-brand_tittle.addEventListener('click',()=>{
-  document.location.href('index.html')
-})
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+const brand_tittle = document.getElementsByClassName("brand-title")[0];
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
+brand_tittle.addEventListener("click", () => {
+  document.location.href("index.html");
+});
