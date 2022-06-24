d1dfe071cd3c2eeb5591d42cda36f64efd1ae8cb
Rajat Kumar Nayak <rajatnayak1582002@gmail.com>
2022-06-24 10:17:15 +0530

    Email Service Added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 const brand_tittle = document.getElementsByClassName("brand-title")[0];
+const input=document.querySelector('form')
+const btn=document.getElementById('send')
+
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
 });
