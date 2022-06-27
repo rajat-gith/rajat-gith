842a6bb79737444e4c8b066c4a78a714caf440c1
Rajat Kumar Nayak <rajatnayak1582002@gmail.com>
2022-06-27 23:21:00 +0530

    commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 const brand_tittle = document.getElementsByClassName("brand-title")[0];
-const input=document.querySelector('form')
-const btn=document.getElementById('send')
+const input=document.querySelector('form');
+const btn=document.getElementById('send');
+const loader=document.getElementById('preloader');
+window.addEventListener("load",function(){
+  loader.style.display="none"
+})
+
+
+
 
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
